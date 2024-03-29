acda4882840e2b6e322c0bd7c6d61d6193f3edb0
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-03-29 13:46:14 -0300

    Implementação do sorteio e da pontuação

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -2,15 +2,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class JogoDaForca {
 
     private final ArrayList<String> palavras;
     private final ArrayList<String> dicas;
+    private final ArrayList<String> letrasDigitadas;
+    private String palavraSorteada;
+    private String dica;
+    private int acertos;
+    private int erros;
 
     public JogoDaForca() throws Exception {
         this.palavras = new ArrayList<>();
         this.dicas = new ArrayList<>();
+        this.letrasDigitadas = new ArrayList<>();
         carregarPalavras();
     }
 
@@ -29,6 +36,46 @@ public class JogoDaForca {
         }
     }
 
+    public String getDica() {
+        return this.dica;
+    }
 
+    public int getTamanho() {
+        return this.palavraSorteada.length();
+    }
 
+    public boolean terminou(){
+        return this.acertos == 6 || this.erros == 6;
+    }
+
+    public void iniciar(){
+        Random rand = new Random();
+        int numRandom = rand.nextInt(this.palavras.size());
+        this.palavraSorteada = this.palavras.get(numRandom);
+        this.dica = this.dicas.get(numRandom);
+    }
+
+    public ArrayList<Integer> getOcorrencias(String letra) throws Exception {
+        letra = letra.toUpperCase();
+
+        if(letra.length() != 1){
+            throw new Exception("Apenas uma letra deve ser informada.");
+        }
+        if (this.letrasDigitadas.contains(letra)) {
+            throw new Exception("Letra já digitada.");
+        }
+        this.letrasDigitadas.add(letra);
+
+        ArrayList<Integer> ocorrencias = new ArrayList<>();
+        for (int i = 0; i < this.palavraSorteada.length(); i++) {
+            if (this.palavraSorteada.charAt(i) == letra.charAt(0)) {
+                ocorrencias.add(i);
+                this.acertos++;
+            }
+        }
+        if (ocorrencias.isEmpty()) {
+            this.erros++;
+        }
+        return ocorrencias;
+    }
 }
